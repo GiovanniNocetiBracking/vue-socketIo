ff29c7c2fa22c0ad6ad13ce17cf0dab0912fa777
Giovanni Noceti <giovanni.noceti@gmail.com>
2024-10-05 03:52:41 -0300

    feat(setup): pinia added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
-<script setup></script>
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const { count } = useCounterStore();
+</script>
 
 <template>
-  <div class="h-screen w-screen flex justify-center items-center">
-    <h1 class="font-bold text-2xl">hello world</h1>
+  <div class="h-screen w-screen flex justify-center items-center flex-col">
+    <h1 class="font-bold text-2xl mb-4">hello world</h1>
+    <h2>counter: {{ count }}</h2>
   </div>
 </template>
 
