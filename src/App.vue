06f0e1a4c26e00156499385f2620f75eb848e195
Giovanni Noceti <giovanni.noceti@gmail.com>
2024-10-05 03:07:21 -0300

    feat(setup): vueRouter@4 added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <div class="h-screen w-screen flex justify-center items-center">
-    <h1 class="font-bold text-2xl">hello world</h1>
-  </div>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
